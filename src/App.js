bdf75a55811e679c6ab69a1270e00845513a862a
Chongmyung Lee <sam.chongmyung@gmail.com>
2024-01-26 16:03:58 +0900

    axios practice

--- src/App.js
+++ src/App.js
@@ -5,23 +5,27 @@
 // => (shoes = {shoes})
 // use data from props => props.nameOfState : props.shoes
 
-// ============================ Main Code =================================================//
+// ============================ import section =================================================//
 import { Button, Navbar, Container, Nav } from "react-bootstrap";
 import "./App.css";
 import { useState } from "react";
 import data from "./data.js";
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import Detail from "./routes/Detail.js";
+import axios from "axios";
 // ============================ App function =================================================//
 function App() {
-  let [shoes] = useState(data);
+  let [shoes, setShoes] = useState(data);
   let navigate = useNavigate();
-  console.log(shoes);
+  let [count, setCount] = useState(1);
+
   return (
     <div className="App">
+      {/* ===================== Nav Section============================== */}
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          {/* ===================== Nav Link Section============================== */}
           <Nav className="me-auto">
             <Nav.Link
               onClick={() => {
@@ -33,7 +37,7 @@ function App() {
 
             <Nav.Link
               onClick={() => {
-                navigate("/detail");
+                navigate("/detail"); //navigate can make it move a page to the given path.
               }}
             >
               Detail
@@ -44,6 +48,7 @@ function App() {
 
       {/* <Link to="/">홈</Link>
       <Link to="/detail">상세페이지</Link> */}
+
       {/* ==================== Routes Section ======================== */}
       <Routes>
         <Route
@@ -59,18 +64,58 @@ function App() {
                   })}
                 </div>
               </div>
+              <button
+                // When button was pressed, it requets data to server
+                onClick={() => {
+                  // 로딩중 UI 띄우기
+                  setCount(count + 1);
+
+                  if (count < 3) {
+                    // 서버에서 데이터 가져오기
+                    axios
+                      .get(
+                        "https://codingapple1.github.io/shop/data" +
+                          (count + 1) +
+                          ".json"
+                      )
+
+                      // get request to url
+                      .then((결과) => {
+                        console.log(결과.data); // get data and save i
+                        // 가져온 데이터를 기존 데이터와 합치기
+                        let copy = [...shoes, ...결과.data]; //두 오브젝트어레이 합치기
+                        // let copy = shoes.concat(결과.data); // concat 방법으로 두 오브젝트 어레이 합치기
+                        setShoes(copy); //shoes 스테이트 업데이트 => 같은 컴포넌트 사용
+                        // 로딩중UI 숨기기
+                      })
+                      .catch(() => {
+                        // if the request failed
+                        console.log("실패함 ㅅㄱ");
+                      });
+                  } else {
+                    alert("더이상 상품은 없습니다.");
+                  }
+                }}
+              >
+                더보기
+              </button>
             </div>
           }
         />
+        {/* 잘못된 경로는 모두 Page not found 보여주기 */}
+        <Route path="*" element={<div>Page not found</div>} />
+
         {/* url 파라미터 detail/아무거나 => element 보여주세요*/}
         <Route path="/detail/:id" element={<Detail shoes={shoes} />}></Route>
 
+        {/*================== Nested routes ========================*/}
         <Route path="event" element={<Event />}>
           <Route
             path="one"
             element={<div>첫 주문시 양배추즙 서비스</div>}
           ></Route>
           <Route path="two" element={<div>생일기념 쿠폰받기</div>}></Route>
+          {/* ============================================== */}
         </Route>
       </Routes>
     </div>
@@ -78,6 +123,7 @@ function App() {
 }
 
 /// ============================ Components ==========================================//
+
 let Event = () => {
   return (
     <div>
@@ -98,7 +144,7 @@ let Card = (props) => {
       />
       {/* Added i props to get count number */}
       <h4>{props.shoes[props.i].title}</h4>
-      <p>{props.shoes[props.i].price}</p>
+      <p>{props.shoes[props.i].price}원</p>
     </div>
   );
 };
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,31 +1,82 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Nav } from "react-bootstrap";
 
 let Detail = (props) => {
+  // ======== 업데이트시 필요한 타이머 스테이트 : true일때 보여준다==========
+
   let { id } = useParams(); //현재 URL의 파라미터를 뽑아줌
-  let 찾은상품 = props.shoes.find(function (x) {
+  let [timer, timerState] = useState(true);
+
+  let foundProduct = props.shoes.find(function (x) {
     return x.id == id; // array자료.id == url에입력한번호
   });
 
+  let [inputValue, inputValueState] = useState("");
+
+  // 페이지가 렌더링될때 실행 => 2초후 timerState를 False로 업데이트
+  useEffect(() => {
+    let a = setTimeout(() => {
+      timerState(false);
+    }, 2000);
+
+    //useEffect 동작 전에 실행됨
+    return () => {
+      // 클리너 코드를 씀
+      // 기존 데이터 요청 제거
+      //기존 타이머는 제거
+      clearTimeout(a);
+    };
+  }, []); // []안에 값이 업데이트 될때(마운트할때) 실행 => 공백일땐 처음 페이지가 렌더링 될때만 실행
+
+  useEffect(() => {
+    //입력창에서 입력값 받아오기
+    if (isNaN(inputValue) == true) {
+      alert("숫자만 입력하세요");
+    }
+  }, [inputValue]);
   return (
     <div className="container">
+      {/* 이벤트창 => 2초후 useState가 false로 바뀜 => 이벤트창은 사라짐 */}
+      {timer === true ? (
+        <div className="alert alert-warning">2초이내 구매시 할인</div>
+      ) : null}
+
       <div className="row">
         <div className="col-md-6">
           <img
             src={
               "https://codingapple1.github.io/shop/shoes" +
-              (props.shoes[id].id + 1) +
+              (foundProduct.id + 1) +
               ".jpg"
             }
             width="100%"
           />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">{props.shoes[id].title}</h4>
-          <p>{props.shoes[id].content}</p>
-          <p>{props.shoes[id].price}</p>
+          <input
+            type="text"
+            onChange={(e) => {
+              inputValueState(e.target.value);
+            }}
+          ></input>
+          <h4 className="pt-5">{foundProduct.title}</h4>
+          <p>{foundProduct.content}</p>
+          <p>{foundProduct.price}</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link eventKey="link0">버튼0</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link11">버튼1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link2">버튼2</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </div>
   );
 };
